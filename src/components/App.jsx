cfe8971815b0d6531d4dd690caf191f696cdbc21
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2024-10-25 15:21:12 +0200

    tabl ahd friends ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import Profile from "./Profile/Profile.jsx";
 import userData from "../userData.json";
 import FriendList from "./FriendList/FriendList.jsx";
+import friends from "../friends.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory.jsx";
+import transactions from "../transactions.json";
+import "./App.css";
 
 const App = () => {
   return (
-    <Profile
-      name={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      image={userData.avatar}
-      stats={userData.stats}
-    />
+    <div className="appContainer">
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
   );
 };
-<FriendList />;
+
 export default App;
